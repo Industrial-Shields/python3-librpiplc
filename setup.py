f9e6f999b95964ea86bf75384b8d1ceb88e3d5bb
Jordi Casals <jordi.casals@gmail.com>
2021-07-31 00:40:54 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+        name = "rpiplc_lib",
+        version = '1.0.0',
+        packages = find_packages(include = ["rpiplc_lib"]),
+        description = "Industrial Shields RPIPLC library for python3",
+        author = "Industrial Shields",
+        license = "LGPL-v3"
+        )
