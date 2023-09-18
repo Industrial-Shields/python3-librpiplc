46c12b3d8550284ad9ece3dbd047b87451edca8f
JoanVC100 <36604048+JoanVC100@users.noreply.github.com>
2023-09-18 08:24:51 +0000

    Bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
         name = "rpiplc_lib",
-        version = '1.0.0',
+        version = '2.0.0',
         packages = find_packages(include = ["rpiplc_lib"]),
         description = "Industrial Shields RPIPLC library for python3",
         author = "Industrial Shields",
