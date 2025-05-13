ec7ce90af7b3440ebd2e01e7c1d72ba958d5c0a6
JoanVC <36604048+JoanVC100jg@users.noreply.github.com>
2025-05-13 14:25:10 +0200

    feat: Add old mapping for GateBerry V9

---
+++ librpiplc/old_gateberry_mapping_v9.py
@@ -0,0 +1,32 @@
+"""
+Copyright (c) 2025 Industrial Shields. All rights reserved
+
+This file is part of python3-librpiplc.
+
+python3-librpiplc is free software: you can redistribute
+it and/or modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, either version
+3 of the License, or (at your option) any later version.
+
+python3-librpiplc is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+from .mapping import PLCMappingDict
+
+extras = {
+	"DE_RE": 27,
+        "EXP_RST": 26,
+        "EXP_CS": 11,
+        "EXP_AN": 16,
+        "EXP_PWM": 20,
+        "EXP_INT": 21,
+}
+
+hw = {
+	"GATEBERRY": PLCMappingDict({**extras})
+}
